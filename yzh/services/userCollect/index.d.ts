32317c6928e4367f1b239e474b0b708241aa9981
shi.yan <shi.yan@yunzhanghu.com>
2025-07-16 17:33:31 +0800

    用户信息采集增加「采集手机号码 」功能

---
+++ yzh/services/userCollect/index.d.ts
@@ -0,0 +1,66 @@
+import YZHclient from '../../common/client';
+/** GetUserCollectPhoneStatusRequest 查询手机号码绑定状态请求 */
+interface GetUserCollectPhoneStatusRequest {
+    /** 平台企业 ID */
+    dealer_id: string;
+    /** 综合服务主体 ID */
+    broker_id: string;
+    /** 平台企业用户 ID */
+    dealer_user_id: string;
+    /** 姓名 */
+    real_name: string;
+    /** 证件号码 */
+    id_card: string;
+    /** 证件类型编码 */
+    certificate_type: number;
+}
+
+/** GetUserCollectPhoneStatusResponse 查询手机号码绑定状态返回 */
+interface GetUserCollectPhoneStatusResponse {
+    /** 手机号码收集 Token */
+    token: string;
+    /** 绑定状态 */
+    status: number;
+}
+
+/** GetUserCollectPhoneURLRequest 获取收集手机号码页面请求 */
+interface GetUserCollectPhoneURLRequest {
+    /** 手机号码收集 Token */
+    token: string;
+    /** 主题颜色 */
+    color: string;
+    /** 回调地址 */
+    url: string;
+    /** 跳转 URL */
+    redirect_url: string;
+}
+
+/** GetUserCollectPhoneURLResponse 获取收集手机号码页面返回 */
+interface GetUserCollectPhoneURLResponse {
+    /** 收集手机号码页面 URL */
+    url: string;
+}
+
+export declare class UserCollectServiceClient extends YZHclient {
+    constructor(conf: {
+        dealer_id: string;
+        broker_id: string;
+        app_key: string;
+        des3_key: string;
+        private_key: string;
+        yzh_public_key: string;
+        sign_type: 'rsa' | 'sha256';
+        base_url?: string;
+        timeout?: number;
+    });
+    GetUserCollectPhoneStatus(
+        req: GetUserCollectPhoneStatusRequest,
+        cb?: (error: null | string, rep: GetUserCollectPhoneStatusResponse) => void
+    ): Promise<GetUserCollectPhoneStatusResponse>;
+    GetUserCollectPhoneURL(
+        req: GetUserCollectPhoneURLRequest,
+        cb?: (error: null | string, rep: GetUserCollectPhoneURLResponse) => void
+    ): Promise<GetUserCollectPhoneURLResponse>;
+}
+
+export {};
